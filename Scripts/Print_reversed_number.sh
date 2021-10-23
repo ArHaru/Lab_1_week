843c0117b45325f279af5fe376cf6d343db92df0
ArHaru <aram.harutyunyan.dec@gmail.com>
2021-10-23 19:13:37 +0400

    The correct number of arguments is checked through 0 and now it works for negative numbers

--- Scripts/Print_reversed_number.sh
+++ Scripts/Print_reversed_number.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-number="$1"
-
-if [ -z $number ]; then
-	echo "No argument was passed"
-	exit 1
+if ! [ $# == 1 ]; then
+        echo "Wrong number of arguments"
+        exit 1
 fi
+
+number="$1"
 
 if ! [[ "$number" =~ ^-?[0-9]+$  ]]; then
 	echo "$number is not a number"
@@ -14,6 +14,11 @@ fi
 
 if [ $number == 0 ]; then
 	echo -n 0
+fi
+
+if [ $number -lt 0 ]; then
+	let number*=-1;
+	echo -n -
 fi
 
 while [ $number -gt 0 ]; do
